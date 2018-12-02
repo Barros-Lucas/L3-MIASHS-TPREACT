5146d207c5e276f099bcaa2ee591b288f0ad85a2
sousouc <lucas.barros@hotmail.fr>
2018-12-02 19:59:56 +0100

    modification des props dans cards pour essayer et début du travail sur le fichier json via la méthode getChampion mais rien s'affiche sans s'avoir pourquoi

--- src/Board.js
+++ src/Board.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
 import './Board.css';
 import Card from './Card';
+import champ from "./Champions.json";
 
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab : []
+
+
+    };
+  }
+
+  getChampions(){
+    var random = 1;
+    for(let id in champ.data.id){
+      console.log(id)
+      console.log(random)
+    }
+}
+    // fetch("champions.json")
+    //     .then(response => response.json())
+    //     .then(response => {
+    //       this.champion = Object.keys(response.data);
+    //
+    //         let randIndex = 1;
+    //         this.randChamp.push({
+    //           name: this.champion[randIndex],
+    //           src: this.champion[randIndex] + "_0.jpg",
+    //           flip:false
+    //         });
+    //       });
+    //       return this.randChamp[0];
+
+coucou(){
+  return <Card></Card>
+}
   render() {
+
+
     return (
-      <section class="card-columns ">
-          <Card/>
+
+      <section className="card-columns ">
+      {this.getChampions()}
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+
+
       </section>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -3,13 +3,22 @@ import './Card.css';
 
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name : "Tresh",
+      url : "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg",
+      isFlipped : false
 
+
+    };
+  }
 
   render() {
     return (
       <div className="card bg-dark text-white border-warning border-3">
-          <img className="card-img" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg"/>
-          <p className="card-text">Thresh.</p>
+          <img className="card-img" src={this.state.url}/>
+          <p className="card-text">{this.state.name}</p>
       </div>
     );
   }
