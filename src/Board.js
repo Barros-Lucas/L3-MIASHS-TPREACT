85aba8032393916173068eb54d84afd340c6e537
sousouc <lucas.barros@hotmail.fr>
2018-12-03 15:03:24 +0100

    Clique sur carte MARCHE!!! (retourner/tourner) (pas de test si nbCarteRetourner <=2)

--- src/Board.js
+++ src/Board.js
@@ -4,10 +4,58 @@ import Card from './Card';
 
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab : [],
+      cartes : []
+
+
+    };
+  }
+
+  getChampions(){
+    fetch('./Champions.json')
+    .then(response => response.json())
+    .then(response =>{
+        for(let champion in response.data){
+          console.log(response.data[champion].id)
+        }
+    });
+}
+
+// makeBoard(){
+//   const champions = this.state.tab;
+//   for(let index=0;index<3;++index ){
+//     console.log(champions[index]);
+//     this.state.setState(this.cartes.push(<Card name={champions[index].name} ></Card>));
+//   }
+// return this.state.cartes
+// }
+makeBoard(){
+  const nb = 6;
+
+    this.state.cartes.push(<Card name='Aatrox' ></Card>);
+    this.state.cartes.push(<Card name='Ahri' ></Card>);
+    this.state.cartes.push(<Card name='Akali' ></Card>);
+    this.state.cartes.push(<Card name='Aatrox' ></Card>);
+    this.state.cartes.push(<Card name='Ahri' ></Card>);
+    this.state.cartes.push(<Card name='Akali' ></Card>);
+
+return this.state.cartes
+}
   render() {
+
+
     return (
-      <section class="card-columns ">
-          <Card/>
+
+      <section className="card-columns ">
+      {this.getChampions()}
+
+      {this.makeBoard()}
+
+
+
       </section>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -3,15 +3,42 @@ import './Card.css';
 
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:this.props.name,
+      nameFlip: "Clique",
+      isFlipped : false,
 
+
+    };
+  }
+
+  onClick = () => {
+     this.setState({
+          name:this.state.name,
+           nameFlip: "Clique",
+           isFlipped : !this.state.isFlipped});
+
+  }
 
   render() {
-    return (
-      <div className="card bg-dark text-white border-warning border-3">
-          <img className="card-img" src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_0.jpg"/>
-          <p className="card-text">Thresh.</p>
-      </div>
-    );
+    if(this.state.isFlipped){
+      return (
+        <div className="card bg-dark text-white border-warning border-3" onClick={this.onClick}>
+            <img className="card-img" src={"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+this.state.name+"_0.jpg"}/>
+            <p className="card-text">{this.state.name}</p>
+        </div>
+      );
+    }else{
+      return (
+        <div className="card bg-dark text-white border-warning border-3" onClick={this.onClick}>
+            <img className="card-img" src={'./card_back.jpg'}/>
+            <p className="card-text">{this.state.nameFlip}</p>
+        </div>
+      );
+    }
+
   }
 }
 
